1367379b69091332ba56a47d559de751b92b7679
ChiomaEm <120136376+ChiomaEmeakama@users.noreply.github.com>
2023-02-03 00:51:41 -0500

    Exercise 1.2

--- Exercises/Chapter1/Program.cs
+++ Exercises/Chapter1/Program.cs
@@ -49,7 +49,7 @@
         //  }
 
         {
-            //Exercise 1:
+            //Exercise 1.1:-------------------------------------------------------
             //Write a program that reads from the console the radius
             //"r" of a circle and prints its perimeter and area.
 
@@ -58,7 +58,35 @@
                     int a = int.Parse(Console.ReadLine());
                 Console.WriteLine("The Perimeter of the circle with radius r = {0} is: ", 2 * Math.PI * a );
             }
-           
+
+            //Exercise 1.2:-------------------------------------------------------
+            //  A given company has name, address, phone number, fax
+            //  number, web site and manager. The manager has name,
+            //  surname and phone number. Write a program that reads
+            //  information about the company and its manager and then
+            //  prints it on the console.
+
+            {
+                Console.WriteLine("Input Name of Company: ");
+                string companyName = Console.ReadLine();
+                Console.WriteLine("Input Company's Address: ");
+                string companyAddress = Console.ReadLine();
+                Console.WriteLine("Input Company's Phone Number: ");
+                string companyPhoneNumber = Console.ReadLine();
+                Console.WriteLine("Input Manager's First Name: ");
+                string managerFirstName = Console.ReadLine();
+                Console.WriteLine("Input Namager's Last Name: ");
+                string managerLastName = Console.ReadLine();
+                Console.WriteLine("Input Manager's Phone number: ");
+                string managerPhoneNumber = Console.ReadLine();
+                Console.WriteLine("Details of Your COmpany and it's Manager: ");
+                Console.WriteLine("Company Name: {0}", companyName);
+                Console.WriteLine("  Company Address: {1}", companyAddress);
+                Console.WriteLine("Company Phone Number: {2}", companyPhoneNumber);
+                Console.WriteLine("Manager's First Name: {3}", managerFirstName);
+                Console.WriteLine("Manager's Last Name: {4}", managerLastName);
+                Console.WriteLine("Manager's Phone Number: {5}", managerPhoneNumber) ;
+            }
         }
     }
 }
